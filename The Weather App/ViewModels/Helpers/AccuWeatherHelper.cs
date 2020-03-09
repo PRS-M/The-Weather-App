cb10b9ddf3d686e102218dd8a45ac6f8306e181b
Mateusz Prusinowski <prusinowski.mateusz@gmail.com>
2020-03-09 23:08:50 +0100

    Add GetCitites Method of type Task<List<City>>

--- The Weather App/ViewModels/Helpers/AccuWeatherHelper.cs
+++ The Weather App/ViewModels/Helpers/AccuWeatherHelper.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
+using The_Weather_App.Models;
 
 namespace The_Weather_App.ViewModels.Helpers
 {
@@ -8,5 +12,28 @@ namespace The_Weather_App.ViewModels.Helpers
     {
         public const string BASE_URL = "http://dataservice.accuweather.com/";
         public const string AUTOCOMPLETE_ENDPOINT = "locations/v1/cities/autocomplete?apikey={0}&q={1}";
+        public const string CURRENT_CONDITIONS_ENDPOINT = "currentconditions/v1/{0}?apikey={1}";
+
+        public static async Task<List<City>> GetCities(string query)
+        {
+            List<City> cities = new List<City>();
+
+            string url = BASE_URL + String.Format(AUTOCOMPLETE_ENDPOINT, API_KEY, query);
+
+            using(HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                string json = await response.Content.ReadAsStringAsync();
+
+                cities = JsonConvert.DeserializeObject<List<City>>(json);
+            }
+
+            return cities;
+        }
+
+        public static async Task<CurrentConditions> GetCurrentConditions(string cityKey)
+        {
+
+        }
     }
 }
