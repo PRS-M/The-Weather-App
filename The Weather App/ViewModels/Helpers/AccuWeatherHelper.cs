30be32ccee658cef5c8fc4ce937411d3ffda97ef
Mateusz Prusinowski <prusinowski.mateusz@gmail.com>
2020-03-09 22:20:30 +0100

    Add AccuWeatherHelper

---
+++ The Weather App/ViewModels/Helpers/AccuWeatherHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace The_Weather_App.ViewModels.Helpers
+{
+    public partial class AccuWeatherHelper
+    {
+        public const string BASE_URL = "http://dataservice.accuweather.com/";
+        public const string AUTOCOMPLETE_ENDPOINT = "locations/v1/cities/autocomplete?apikey={0}&q={1}";
+    }
+}
