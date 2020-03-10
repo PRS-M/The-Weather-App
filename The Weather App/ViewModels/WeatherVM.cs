a3653e263f80217c59ed1fc10d535b306e8c81c6
Mateusz Prusinowski <prusinowski.mateusz@gmail.com>
2020-03-10 23:25:41 +0100

    Add Properties, StaticResources, DataContext and Binding

--- The Weather App/ViewModels/WeatherVM.cs
+++ The Weather App/ViewModels/WeatherVM.cs
@@ -2,11 +2,50 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Text;
+using The_Weather_App.Models;
 
 namespace The_Weather_App.ViewModels
 {
     public class WeatherVM : INotifyPropertyChanged
     {
+        private string query;
+
+        public string Query
+        {
+            get { return query; }
+            set
+            {
+                query = value;
+                OnPropertyChanged("Query");
+            }
+        }
+
+        private CurrentConditions currentConditions;
+
+        public CurrentConditions CurrentConditions
+        {
+            get { return currentConditions; }
+            set
+            {
+                currentConditions = value;
+                OnPropertyChanged("CurrentConditions");
+            }
+        }
+
+        private City selectedCity;
+
+        public City SelectedCity
+        {
+            get { return selectedCity; }
+            set
+            {
+                selectedCity = value;
+                OnPropertyChanged("SelectedCity");
+            }
+        }
+
+
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         private void OnPropertyChanged(string propertyName)
