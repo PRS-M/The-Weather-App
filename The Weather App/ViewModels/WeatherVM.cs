60e3bc119af34e6747a66659b3ccda69f7d7e401
Mateusz Prusinowski <prusinowski.mateusz@gmail.com>
2020-03-10 22:24:34 +0100

    Add VM for Weather

---
+++ The Weather App/ViewModels/WeatherVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace The_Weather_App.ViewModels
+{
+    public class WeatherVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
